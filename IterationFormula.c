e2a7405aa7c51a888481e712c796d3063d89d7bd
Павел Левицкий <creative-creative@outlook.com>
2023-12-19 19:30:12 +0700

    upd: преобразование

--- IterationFormula.c
+++ IterationFormula.c
@@ -2,24 +2,15 @@
 #include <math.h>
 #include <stdlib.h>
 #include <float.h>
+#include <conio.h>
+#include <windows.h>
 
-int factorial(float value)
+float iterationFormula(float qp, int n, float x)
 {
-    if (value == 0)
-        return 1;
-    else
-        return value * factorial(value - 1);
+	return (x / (2 * n + 2)) * qp;
 }
 
-float iterationFormula(float n, float x)
-{
-	float firstExponent = n + 1;
-	float secondExponent = 2 * n + 1;
-	float thirdExponent = 4 * n + 2;
-	return (pow(-1, firstExponent) / factorial(secondExponent)) * (pow(x, thirdExponent) / n);
-}
-
-void error()
+void fail()
 {
 	printf("\nВы все сломали. В следующий раз вводите корректные значения.");
 	exit(1);
@@ -27,30 +18,27 @@ void error()
 
 int main()
 {
-	system("chcp 1251");
+	SetConsoleOutputCP(1251);
 	float eps = 0.0, x = 0.0;
-	int isDigit = 0;
 	printf("\nВведите значение точности, большее -1:\t");
-	isDigit = scanf("%f", &eps);
-	if(isDigit != 1 || eps <= -1)
-        error();
+	scanf_s("%f", &eps);
 	int overflow = 0;
 	int n = 1;
-	float q1, q2 = 0.0;
-	float s = 0;
+	float q1 = 0.0;
+	float q2 = 0.0;
+	float s = 0.0;
 
-	printf("\nВведите x:\t");
-	isDigit = scanf("%f", &x);
-	if (isDigit != 1)
-        error();
-	q1 = iterationFormula(n, x);
+	if (eps <= -1) fail();
+	printf_s("\nВведите x:\t");
+	scanf_s("%f", &x);
+	q1 = iterationFormula(1., n, x);
 	s += q1;
 
 	while ((fabs(q1 - q2) > eps) & (overflow == 0))
 	{
 		q2 = q1;
 		n++;
-		q1 = iterationFormula(n, x);
+		q1 = iterationFormula(1., n, x);
 		if (n % 2 == 0) s += q1;
 		else s -= q1;
 
@@ -58,8 +46,8 @@ int main()
 			overflow = 1;
 	}
 	if (overflow == 0)
-		printf("Результат %f достигнут за %u итераций\n", s, n);
+		printf("Результат %f был достигнут за %u итераций", s, n);
 	else
-		printf("\nУказанная точность недостижима, %u\n", n);
-	printf("Работа выполнена Левицким Павлом, группа 423-2");
+		printf("\nУказанная точность недостижима, %u итераций\n", n);
+	printf("\nРабота выполнена Левицким Павлом, группа 423-2");
 }
